db1648423a422686799276d675c69204a49af56d
Radoslaw Bojba <radoslaw.bojba@di-support.com>
2019-02-13 12:00:20 +0100

    cleanup project. refactoring. bare minimum version.

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,8 @@
-FROM openjdk:8-jre-alpine
+FROM adoptopenjdk/openjdk8-openj9:alpine-slim
 VOLUME /tmp
-ADD 'target/docker-project-0.0.1-SNAPSHOT.jar' app.jar
+
+ADD 'target/app.jar' app.jar
+ADD target/thin.properties thin.properties
+
 EXPOSE 8080:8080
-ENTRYPOINT [ "java", "-Xss256k", "-Xmx32m", "-XX:+UnlockExperimentalVMOptions", "-Djava.security.egd=file:/dev/./urandom", "-jar", "/app.jar" ]
+ENTRYPOINT [ "java", "-XX:+UseSerialGC", "-Xss256k", "-Xmx32m", "-XX:MaxRAM=64m" ,"-XX:MaxDirectMemorySize=2M", "-XX:MaxRAMFraction=1", "-XX:+UnlockExperimentalVMOptions", "-Djava.security.egd=file:/dev/./urandom", "-jar", "/app.jar", "--thin.root=/m2" ]
--- src/main/java/pl/radomiej/dockerproject/DockerProjectApplication.java
+++ src/main/java/pl/radomiej/dockerproject/DockerProjectApplication.java
@@ -5,26 +5,12 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
-import org.springframework.data.redis.core.StringRedisTemplate;
-import pl.radomiej.dockerproject.service.redis.RedisEventBusService;
 
 @SpringBootApplication
 public class DockerProjectApplication {
-    private static final Logger LOGGER = LoggerFactory.getLogger(RedisEventBusService.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(DockerProjectApplication.class);
 
-    public static void main(String[] args) throws InterruptedException {
-        //Hack for fast deployment in kubernatest app wizard
-        String[] fixSingleArg = args;
-        if(args.length == 1) fixSingleArg = args[0].split(" ");
-
-
-        ApplicationContext ctx = SpringApplication.run(DockerProjectApplication.class, fixSingleArg);
-        StringRedisTemplate template = ctx.getBean(StringRedisTemplate.class);
-
-        LOGGER.info("Sending message...");
-        template.convertAndSend("chat", "Hello from Redis!");
-
-
-
+    public static void main(String[] args) {
+        ApplicationContext ctx = SpringApplication.run(DockerProjectApplication.class, args);
     }
 }
